92fcac3f15e463f68cdcdf5d5738f021bfe8d4ee
KazukiChiyo <kexuanzou@gmail.com>
2018-07-31 20:55:31 -0500

    model should work; doens't pass muddy road

--- drive.py
+++ drive.py
@@ -3,7 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
-
+import cv2
 import numpy as np
 import socketio
 import eventlet
@@ -61,6 +61,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.GaussianBlur(image_array, (3, 3), 0)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2YUV)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
